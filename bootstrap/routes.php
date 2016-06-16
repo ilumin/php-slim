2a1fc62ebcf140595c7b9468b339d7826e674311
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-16 17:43:05 +0700

    UPDATE routes for update product

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -4,3 +4,4 @@ $app->get('/hello/{name}', 'SimpleCrudAction:hello');
 $app->get('/products', 'SimpleCrudAction:fetch');
 $app->get('/products/{id}', 'SimpleCrudAction:get');
 $app->post('/products', 'SimpleCrudAction:create');
+$app->put('/products/{id}', 'SimpleCrudAction:update');
