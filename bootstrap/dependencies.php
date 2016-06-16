a62b9c4a57a7584ae7be6976e01aff6d0a7006f0
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-16 17:01:07 +0700

    UPDATE dependencies

--- bootstrap/dependencies.php
+++ bootstrap/dependencies.php
@@ -2,6 +2,12 @@
 
 $container = $app->getContainer();
 
+$container['doctrine'] = function ($c) {
+    $entityManager = require BOOTSTRAP . '/doctrine.php';
+    return $entityManager;
+};
+
 $container['SimpleCrudAction'] = function ($c) {
-    return new App\Action\CrudAction();
+    $productResource = new App\Resource\ProductResource($c->get('doctrine'));
+    return new App\Action\CrudAction($productResource);
 };
