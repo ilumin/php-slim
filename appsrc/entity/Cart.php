c9d9a1ff26e858ab76828586e6698e844f3857e6
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-20 16:06:43 +0700

    ADD cart entity

---
+++ appsrc/entity/Cart.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="carts")
+ */
+class Cart
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @ORM\Column(type="float", name="total_price")
+     * @var float
+     */
+    public $totalPrice;
+    public $id;
+
+    /**
+     * @ORM\Column(type="int", name="item_count")
+     * @var float
+     */
+    public $itemCount;
+
+    /**
+     * @ORM\Column(type="datetime", name="created_at")
+     * @var \DateTime
+     */
+    protected $createdAt;
+}
